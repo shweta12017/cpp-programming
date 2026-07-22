d97c4f30d8d0fd16c94b655dc3151c08b7189f4f
shweta12017 <shwetadesai4109@gmail.com>
2026-07-22 10:40:12 +0530

    Add files via upload

---
+++ largest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a, b, c;
+cout<<"enter the 3 numbers:";
+cin>>a>>b>>c;
+if(a>=b&&a>=c){
+cout<<"largest number="<<a<<endl;
+}else if(b>=a&&b>=c){
+cout<<"largest number="<<b<<endl;
+}else{
+cout<<"largest number="<<c<<endl;
+}
+return 0;
+}
