da71fa1d0587fed11b35c56bcde068bccafc884e
shweta12017 <shwetadesai4109@gmail.com>
2026-07-29 09:03:17 +0000

    change

--- switch.cpp
+++ switch.cpp
@@ -8,21 +8,22 @@ int main()
     cout<<"2. check palindrome";
     cout<<"3. check armstrong";
     cout<<"enter the choice";
+    cin>>choice;
     cout<<"Enter the number:";
     cin>>n;
     switch(choice){
         case 1:{
             int rev=0, temp=n;
-            while (temp>0){
+            while (temp!=0){
         int reminder=temp%10;
         rev=rev*10+reminder;
         temp=temp/10;
     }
-    cout<<"reverse number"<<rev;
+    cout<<"reverse number:"<<rev;
     break;
         }case 2:{
             int rev=0, temp=n;
-            while (temp>0){
+            while (temp!=0){
         int reminder=temp%10;
         rev=rev*10+reminder;
         temp=temp/10;
@@ -33,7 +34,7 @@ int main()
     }break;
         }case 3:{
             int temp=n, digit=0, result=0, reminder;
-            while(n!=0){
+            while(temp!=0){
         digit++;
         temp=temp/10;
     }temp=n;
