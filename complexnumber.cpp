7ca25a5eb87ccf23c73d1f5f77e776c449c213b4
shweta12017 <shwetadesai4109@gmail.com>
2026-08-18 09:55:28 +0000

    change

---
+++ complexnumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+    private:
+    int real;
+    int img;
+    public:
+    void accept()
+    {
+        cout<<"enter real part";
+        cin>>real;
+        cout<<"enter imaginary part";
+        cin>>img;
+    }
+    void add(complex c1, complexc2)
+}
