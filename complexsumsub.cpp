4dd5712aa7181c1c04c393ac3006bc44257f9e66
shweta12017 <shwetadesai4109@gmail.com>
2026-08-18 16:04:07 +0000

    change

---
+++ complexsumsub.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+    private:
+    int real, imag;
+    public:
+    void accept()
+    {
+        cin>>real;
+        cin>>imag;
+    }
+    void add(complex c1, complex c2)
+    {
+        real = c1.real + c2.real;
+        imag = c1.imag + c2.imag;
+    }
+    void sub(complex c1, complex c2)
+    {
+        real = c1.real - c2.real;
+        imag = c1.imag - c2.imag;
+    }
+    void display()
+    {
+        cout << real << " + " << imag << "i";
+    }
+};
+int main()
+{
+    complex c1, c2, c3, c4;
+
+    cout << "Enter first complex number (real imaginary): ";
+    c1.accept();
+
+    cout << "Enter second complex number (real imaginary): ";
+    c2.accept();
+
+    c3.add(c1, c2);
+    c4.sub(c1, c2);
+
+    cout << "\nAddition = ";
+    c3.display();
+
+    cout << "\nSubtraction = ";
+    c4.display();
+
+    return 0;
+}
